b6cc368b59b217fe0236c182dede28bedd74832c
i-yuka1129 <y.izumisawa1129@gmail.com>
2023-10-26 13:15:25 +0900

    33. 学習に使用するデータについて知ろう 34. データを取得して1件表示しよう  - ``` useFetch('URL') ```：コンポーサブル。非同期データ取得APIのうちの1つ  ※Nuxt3ドキュメントでその他のオプションを確認できる 35. 複数のデータを表示しよう 36. エラー処理をしよう

--- app.vue
+++ app.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <ul>
+      <li v-for="user in users" :key="user.id">
+      {{ user.id }}, {{ user.name }}
+      </li>
+    </ul>
   </div>
 </template>
+<script setup>
+  const { data: users, error } = await useFetch('https://jsonplaceholder.typicode.com/usersssss')
+  // console.log(error.value)
+  if(error.value) {
+    throw createError({ statusCode: 404, statusMessage: 'Page not found!!!'})
+  }
+</script>
